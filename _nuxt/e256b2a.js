8df300e1a6c28971d0cd7277c1b80b3f47f657a7
okex-dapp <109569925+okex-dapp@users.noreply.github.com>
2022-09-09 17:16:04 +0800

    Add files via upload

---
+++ _nuxt/e256b2a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{2232:function(n,e,c){"use strict";c.r(e);var t={fetch:function(n){(0,n.redirect)("https://docs.alcor.exchange")}},d=c(12),component=Object(d.a)(t,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
